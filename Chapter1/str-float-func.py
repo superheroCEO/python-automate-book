4f45171bab3481a22dd6df743e825be8daa068c7
superheroCEO <ja@bringbackfit.com>
2016-07-12 14:31:45 -0700

    ch1 files consolidated to new folder

---
+++ Chapter1/str-float-func.py
@@ -0,0 +1,39 @@
+str(13)
+'13'
+print('I am ' + str(13) + ' years old.')
+I am 13 years old.
+
+str(0)
+'0'
+str(-3.13)
+'-3.13'
+int('42')
+42
+float('3.13')
+3.13
+
+spam = input()
+101
+spam
+'101'
+
+spam = int(spam)
+spam
+101
+
+spam * 10 / 5
+202.0
+
+int(7.7)
+7
+int(7.7) + 1
+8
+
+print('What is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge)) + 1) + ' in a year.')
+
+42 == '42'
+False
+42 == 42.0
+True
